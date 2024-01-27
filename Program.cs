2d634b6a20e3cf7c2c8c7f1dfb14e394b71a3e05
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-27 10:56:02 +0000

    CFB22.2

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿namespace aula22._2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] num = new int[5] { 11, 22, 33, 44, 55 };
+
+            foreach(int i in num)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
+        
+ }
